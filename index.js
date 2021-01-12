5a542660187d623427b490cd94dc11642653fd92
danajackson2 <danajackson2@gmail.com>
2021-01-12 11:44:04 -0800

    uppercase test is goofy

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = "Willow"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Clark"
+    return leastFavoriteCustomer
+}
